61165ecfa188245646f6bba848ae33aa56a357c4
Liviu Ionesi <pehlivanu@gmail.com>
2024-03-14 23:48:25 +0100

    save latest changes before attempting to update the dependecies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
-const App = () => {
-  return (
-    <div className="App">
-      <h1> App.jsx</h1>
-    </div>
-  );
-};
+import React from "react";
+import { CssBaseline } from "@mui/material";
+
+const App = () => (
+  <div className="App">
+    <CssBaseline />
+    <h1> App.jsx test</h1>
+  </div>
+);
 
 export default App;
