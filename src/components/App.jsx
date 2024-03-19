1a41451ecaa4cfddecfb3752965e38c0e7f15107
Liviu Ionesi <pehlivanu@gmail.com>
2024-03-19 21:29:51 +0100

    created styles using makeStyle  hook in styles.js and used them using useSytles hook in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,25 @@ import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
 import { Actors, MovieInformation, Movies, Profile, Navbar } from '.';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <Navbar />
-    <main>
-      <Switch>
-        <Route exact path="/"><Movies /></Route>
-        <Route exact path="/movies/:id"><MovieInformation /></Route>
-        <Route exact path="/actors/:id"><Actors /></Route>
-        <Route exact path="/profiles/:id"><Profile /></Route>
-      </Switch>
-    </main>
-  </div>
-);
+import useStyles from './styles';
 
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/"><Movies /></Route>
+          <Route exact path="/movies/:id"><MovieInformation /></Route>
+          <Route exact path="/actors/:id"><Actors /></Route>
+          <Route exact path="/profiles/:id"><Profile /></Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 export default App;
