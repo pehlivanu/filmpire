2affb38c726f5894f777d1118444b0ae6078965b
Liviu Ionesi <pehlivanu@gmail.com>
2024-03-19 21:03:32 +0100

    Added the components in the app routes and placed the navbar inside the app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
+import { Actors, MovieInformation, Movies, Profile, Navbar } from '.';
 
 const App = () => (
   <div>
     <CssBaseline />
+    <Navbar />
     <main>
       <Switch>
-        <Route exact path="/"><h1>Movies</h1></Route>
-        <Route exact path="/movies/:id"><h1>Movie information</h1></Route>
-        <Route exact path="/actors/:id"><h1>Actors</h1></Route>
-        <Route exact path="/profiles/:id"><h1>Profile</h1></Route>
+        <Route exact path="/"><Movies /></Route>
+        <Route exact path="/movies/:id"><MovieInformation /></Route>
+        <Route exact path="/actors/:id"><Actors /></Route>
+        <Route exact path="/profiles/:id"><Profile /></Route>
       </Switch>
     </main>
   </div>
