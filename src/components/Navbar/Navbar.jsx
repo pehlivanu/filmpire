76809bbf64452f159a4bca4265cca2191791f250
Liviu Ionesi <pehlivanu@gmail.com>
2024-03-19 23:31:45 +0100

    Defined the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import { AppBar, Avatar, IconButton, Toolbar, useMediaQuery, Button } from '@mui/material'; // Drawer
+import { Menu, Brightness4, Brightness7, AccountCircle } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import useTheme from '@mui/material/styles';
+import useStyles from './styles';
 
 const Navbar = () => {
-  console.log('Navbar');
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width: 600px)');
+  const theme = useTheme();
+  const isAuthenticated = true;
 
   return (
-    <div>Navbar</div>
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton color="inherit" edge="start" style={{ outline: 'none' }} onClick={() => {}} className={classes.menuButton}>
+              <Menu />
+            </IconButton>
+          )}
+
+          <IconButton color="inherit" sx={{ ml: 1 }} style={{ outline: 'none' }} onClick={() => {}}>
+            {theme.pallete.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && 'Search...'}
+          <div>
+            {!isAuthenticated ? <Button color="inherit" onClick={() => {}}>Login &npsp; <AccountCircle /></Button> : (
+              <Button color="inherit" component={Link} to="/profile/:id" className={classes.linkButton} onClick={() => {}}>
+                {!isMobile && <>My Movies &nbsp; </>}
+                <Avatar style={{ width: 30, height: 30 }} alt="Profile" src="https://media.istockphoto.com/id/587805156/de/vektor/profilbildvektor-illustration.jpg?s=2048x2048&w=is&k=20&c=tpy1jmyRqBdwTLV2nOKNMH-tytpQWnRtg_m3AtXg7gE=" />
+              </Button>
+            )}
+          </div>
+          {isMobile && 'Search...'}
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,26 @@
+import { makeStyles } from '@mui/styles';
+// the () => ({}) means instantly return an object, instead of defining a callback function, which makestyles expects as argument
+export default makeStyles((theme) => ({
+
+  toolbar: {
+    height: '80px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginLeft: '224px',
+    // On mobile devices only
+    [theme.breakpoint.down('sm')]: {
+      marginLeft: 0,
+      flexWrap: 'wrap',
+    },
+  },
+
+  menuButton: {
+    // this ensure equal spacing on every device
+    marginRight: theme.spacing(2),
+    // on non mobile devices it's hidden
+    [theme.breakpoint.up('sm')]: {
+      display: 'none',
+    },
+  },
+
+}));
