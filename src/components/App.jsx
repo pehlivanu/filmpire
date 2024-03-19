eae0839259ac31d9f18458768f7275d8ec664968
Liviu Ionesi <pehlivanu@gmail.com>
2024-03-19 20:42:14 +0100

    created the basic routing for the app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
-  <div className="App">
+  <div>
     <CssBaseline />
-    <h1> App.jsx test</h1>
+    <main>
+      <Switch>
+        <Route exact path="/"><h1>Movies</h1></Route>
+        <Route exact path="/movies/:id"><h1>Movie information</h1></Route>
+        <Route exact path="/actors/:id"><h1>Actors</h1></Route>
+        <Route exact path="/profiles/:id"><h1>Profile</h1></Route>
+      </Switch>
+    </main>
   </div>
 );
 
