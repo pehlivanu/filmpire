2fc7fb60c6749a1dc716e76abc7b8dc8c6d613d7
Liviu Ionesi <pehlivanu@gmail.com>
2024-03-22 14:30:52 +0100

    done

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 //      --url 'https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key=ae8fa6e23866cb34a49337b233547834'
 
-const tmdbApiKey = process.env.TMDB_KEY;
+const tmdbApiKey = process.env.REACT_APP_IMDB_KEY;
 const page = 1;
 
-export const tmbdApi = createApi({
-  reducerPath: 'tmbdApi',
+export const tmdbApi = createApi({
+  reducerPath: 'tmdbApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
   endpoints: (builder) => ({
     //* Get Movies by [Type]
@@ -15,6 +15,7 @@ export const tmbdApi = createApi({
   }),
 });
 
+//* This hook is created autmatically by Redux Toolkit Query, and must have excatly this name use+GetMovies+Query
 export const {
   useGetMoviesQuery,
-} = tmbdApi;
+} = tmdbApi;
